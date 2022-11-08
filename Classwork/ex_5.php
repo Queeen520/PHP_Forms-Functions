733ae3f7fd0855fbf55455d1d1c3e8833c34f362
Isabella W <isabellawindhaber@gmail.com>
2022-11-08 11:22:43 +0100

    ...

---
+++ Classwork/ex_5.php
@@ -0,0 +1,20 @@
+<?php 
+//Create a function that will return the number of minutes, in hours and minutes. 
+//The function should accept only one argument.
+
+//E.g. If we call the function and pass the number of minutes 200 
+//we should get the message "200 minutes = 3 hour(s) and 20 minute(s)."
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ Classwork/ex_6.php
@@ -0,0 +1,21 @@
+<?php 
+/* Create a form with input fields for your firstname, lastname and age. 
+Extract the values from each input field and display them all in a separate div. 
+Use the POST method.
+If the length of your name is larger than 5 characters, 
+change the text colour to green. Otherwise, change the text-colour to red.
+Now extend with adding a new input field "hobbies". Use the GET method to extract and display the hobbies */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
