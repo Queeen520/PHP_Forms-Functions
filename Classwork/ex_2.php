c9069917bb36cf1eb4b9fa74e2c098650ad9a8e2
Isabella W <isabellawindhaber@gmail.com>
2022-11-08 10:56:36 +0100

    ...

---
+++ Classwork/ex_2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<!--Create a function which takes two integer parameters - 
+divide them and output the result on the screen.-->
+
+<?php 
+function divide($int1, $int2) {
+    echo $int1 / $int2;
+}
+echo divide(30,5)." is your solution";
+
+?>
+</body>
+</html>
