a3a194323e205195ced1b34c7c816b3450eb1bbb
Isabella W <isabellawindhaber@gmail.com>
2022-11-08 12:01:03 +0100

    ...

--- Classwork/ex_5.php
+++ Classwork/ex_5.php
@@ -4,6 +4,14 @@
 
 //E.g. If we call the function and pass the number of minutes 200 
 //we should get the message "200 minutes = 3 hour(s) and 20 minute(s)."
+
+function convertMinutes($min) {
+
+    $hours = intdiv($min, 60)." "."hours"." ".($min % 60)." "."minutes";
+    return $min." minutes = ".$hours;
+}
+
+$returnOutput = convertMinutes(500);
 ?>
 
 <!DOCTYPE html>
@@ -15,6 +23,10 @@
     <title>Document</title>
 </head>
 <body>
-    
+
+<?php 
+echo "<h2>".$returnOutput."</h2>";
+?>
+
 </body>
 </html>
